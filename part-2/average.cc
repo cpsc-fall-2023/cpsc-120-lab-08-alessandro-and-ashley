6b9f2d3bea5519998d14110817114ce5a0a3b7df
Ashley Alvarado <AshleyAlvarado@csu.fullerton.edu>
2023-10-29 20:39:03 -0700

    passed all lint, format and unit tests

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Ashley Alvarado
+// AshleyAlvarado@csu.fullerton.edu
+// @AshleyAlvar
+// Partners: @alegarcia4
 
 #include <iostream>
 #include <string>
@@ -7,35 +10,24 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
-  if (arguments.size() > 2) {
-    std:: cout << "error: you must supply at least one number";
-    return -1;
+  if (arguments.size() < 2) {
+    std::cout << "error: you must supply at least one number \n";
+    return 1;
   }
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
-  double sum{0};
-  int num{1};
-  for (num < arguments.size(); ++num) {
-    sum += std::stod(arguments.at(num));
+  bool zero{true};
+  double sum{0.00000};
+  for (std::string& argue : arguments) {
+    if (zero) {
+      zero = false;
+      continue;
+    }
+    sum += std::stod(argue);
   }
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
-  double average{sum/num};
 
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
-  std::cout << "average = " << average;
+  double average{sum / (static_cast<int>(arguments.size()) - 1)};
+
+  std::cout << "average = " << average << "\n";
 
   return 0;
 }
